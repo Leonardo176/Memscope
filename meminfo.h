7b076cfeb42d23c31d2e8c2d6142d7ac76071920
Leonardo Mazzon <leonardomazzon5@gmail.com>
2024-06-15 21:12:05 +0200

    Added some comments

--- meminfo.h
+++ meminfo.h
@@ -4,13 +4,18 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+// The structure for holding memory info
 struct meminfo {
 	void* addr;
 	size_t size;
 };
 
+// Functions that can be used on the meminfo structure
 struct meminfo_f {
+	// Init: initialize the structure given the address and the size of memory
 	struct meminfo (*init) (void* addr, size_t size);
+
+	// isValid: checks if the info of the memory are valid (addr != NULL && size > 0)
 	bool (*isValid) (struct meminfo meminfo);
 };
 
--- memscope.h
+++ memscope.h
@@ -5,8 +5,12 @@
 
 #include "meminfo.h"
 
+/** Main header of the memscope library!
+ * This library allows you to get memory from the heap without worring to remember to use free because it will free it automatically for you!
+ * Calling the function Memscope.alloc() you can get n bytes of memory that you can use inside the passed function!
+**/
 struct memscope_f {
-	void (*alloc)(size_t, void (*)(struct meminfo memory));
+	void (*alloc)(size_t n, void (*)(struct meminfo memory));
 };
 
 extern const struct memscope_f Memscope;
